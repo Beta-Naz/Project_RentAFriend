fbb996168e4da062acfaf6a8a85db17e863f9b55
Beta-Naz <sabitovnazir6@gmail.com>
2026-06-07 21:27:08 +0500

    #Version 0.4.1.2 Beta Адаптация MainWindow под проект. Сабитов

--- RentAFriendApp/MainWindow.xaml.cs
+++ RentAFriendApp/MainWindow.xaml.cs
@@ -1,13 +1,5 @@
-﻿using System.Text;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace RentAFriendApp
 {
@@ -16,9 +8,96 @@ namespace RentAFriendApp
     /// </summary>
     public partial class MainWindow : Window
     {
+        private int? _currentUserId;
+        private string? _currentUserRole;
+        private string? _currentUserName;
+        public static MainWindow? Instanse { get; private set; }
         public MainWindow()
         {
             InitializeComponent();
+            Instanse = this;
+            if (Application.Current.Properties.Contains("CurrentUserId"))
+            {
+                SetupNavigationByRole();
+                UpdateUserInfo();
+            }
+            else
+            {
+                ShowLoginWindow();
+            }
+        }
+
+        private void SetupNavigationByRole()
+        {
+            //switch (_currentUserRole)
+            //{
+            //    case "Client":
+            //        MainFrame.Navigate(new Views.Client.ClientHomePage(_currentUserId));
+            //        Title = "RentAFriend - Клиент";
+            //        break;
+            //    case "Friend":
+            //        MainFrame.Navigate(new Views.Friend.FriendHomePage(_currentUserId));
+            //        Title = "RentAFriend - Друг";
+            //        break;
+            //    case "Admin":
+            //        MainFrame.Navigate(new Views.Admin.AdminDashboardPage(_currentUserId)); ;
+            //        Title = "RentAFriend - Администратор";
+            //        break;
+            //}
+        }
+
+        private void UpdateUserInfo()
+        {
+            CurrentUserName.Text = _currentUserName;
+
+            string? roleDisplay = null;
+            switch (_currentUserRole)
+            {
+                case "Client": roleDisplay = "Клиент"; break;
+                case "Friend": roleDisplay = "Друг"; break;
+                case "Admin": roleDisplay = "Администратор"; break;
+            }
+            if (string.IsNullOrEmpty(roleDisplay))
+            {
+                roleDisplay = "Пользователь";
+            }
+            CurrentUserRole.Text = roleDisplay;
+        }
+
+        private void BtnLogout_Click(object sender, RoutedEventArgs e)
+        {
+            var result = MessageBox.Show("Вы уверены, что хотите выйти?", "Выход",
+                MessageBoxButton.YesNo, MessageBoxImage.Question);
+
+            if (result == MessageBoxResult.Yes)
+            {
+                // Очищаем данные пользователя
+
+                ShowLoginWindow();
+            }
+        }
+
+        private void ShowLoginWindow()
+        {
+            //var loginWindow = new Views.Auth.LoginWindow();
+            //loginWindow.Show();
+            //this.Close();
+        }
+
+        private void Profile_Click(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            //switch (_currentUserRole)
+            //{
+            //    case "Client":
+            //        MainFrame.Navigate(new Views.Client.ClientHomePage(_currentUserId));
+            //        break;
+            //    case "Friend":
+            //        MainFrame.Navigate(new Views.Friend.FriendHomePage(_currentUserId));
+            //        break;
+            //    case "Admin":
+            //        MainFrame.Navigate(new Views.Admin.AdminDashboardPage(_currentUserId)); ;
+            //        break;
+            //}
         }
     }
 }
