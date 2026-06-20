d88f34223a672aab6bbf41a5aa7f220787f1356f
Beta-Naz <sabitovnazir6@gmail.com>
2026-06-20 12:50:12 +0500

    #Версия 0.7.3 Beta

---
+++ RentAFriendApp/Models/DateToDayOfWeekConverter.cs
@@ -0,0 +1,27 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+
+namespace RentAFriendApp.Converters
+{
+    public class DateToDayOfWeekConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is DateTime date)
+            {
+                string[] daysOfWeek = { "Пн", "Вт", "Ср", "Чт", "Пт", "Сб", "Вс" };
+                int dayIndex = (int)date.DayOfWeek;
+                if (dayIndex == 0) dayIndex = 6; 
+                else dayIndex--;
+
+                return daysOfWeek[dayIndex];
+            }
+            return string.Empty;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
